7e45c5efa5d8bc9765aed6d8ff945622087c0674
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-08-09 23:52:16 +0200

    fixes befor deploy to heroku

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(expensesValidator());
 app.use('/', router);
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log('Server listens on port: ' + port);
