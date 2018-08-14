e5c206ef822023f444b699539a0b36e4d3ec7402
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-08-14 15:48:36 +0200

    path fix

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const cors = require('cors');
 const expensesValidator = require('express-validator');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../client'));
+app.set('views', path.join(__dirname, '../client/public'));
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(expensesValidator());
