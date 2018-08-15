6799ce93ba2f804c7eeef567be2f7bba703e57cd
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-08-16 01:35:05 +0200

    updated README.md and removed console.logs

--- server/server.js
+++ server/server.js
@@ -7,24 +7,12 @@ const app = express();
 const cors = require('cors');
 const expensesValidator = require('express-validator');
 
-// app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client/public'));
 app.use(cors());
-// app.use(express.static(path.join(__dirname, '../client/public')));
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    // app.get('*', function(req, res) {
-    //     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    //   });
 }
-
-
-
-
-
-
-
-
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
